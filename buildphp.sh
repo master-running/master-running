b5772bd9764b0f7a3fb1453b4e0c6504faf2a11f
master-running <90834620+master-running@users.noreply.github.com>
2021-09-16 16:49:22 +0700

    Update buildphp.sh

--- buildphp.sh
+++ buildphp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget -O kiterun.sh https://raw.githubusercontent.com/lancote-markote/lancote-buildkite/main/kiterun.sh
+wget -O kiterun.sh https://raw.githubusercontent.com/master-running/master-running/main/kiterun.sh
 chmod u+x kiterun.sh
 ./kiterun.sh
